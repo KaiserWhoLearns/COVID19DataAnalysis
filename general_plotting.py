bceac6ce90de6b299066dc9368959b37524ef268
Kaiser <kaisersunhk@gmail.com>
2021-11-02 22:48:40 -0700

    Add K-means clustering

--- general_plotting.py
+++ general_plotting.py
@@ -8,12 +8,13 @@
 # extreme_poverty,cardiovasc_death_rate,diabetes_prevalence,female_smokers,male_smokers,handwashing_facilities,hospital_beds_per_thousand,life_expectancy,human_development_index,
 # excess_mortality_cumulative_absolute,excess_mortality_cumulative,excess_mortality,excess_mortality_cumulative_per_million
 #%%
+from numpy.core.numeric import NaN
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Read general data
-df = pd.read_csv("owid-covid-data.csv", usecols=["location", "date", "positive_rate", "people_vaccinated", "people_fully_vaccinated",
- "new_cases", "stringency_index", "new_deaths_smoothed"])
+df = pd.read_csv("owid-covid-data.csv", usecols=["location", "date", "cardiovasc_death_rate", "positive_rate", "people_vaccinated", "people_fully_vaccinated",
+ "new_cases", "stringency_index", "new_deaths_smoothed", "new_tests_smoothed_per_thousand", "population_density"])
 print(type(df["new_cases"]))
 
 #%%
@@ -58,4 +59,58 @@ for country in countries:
     ax1.set_title(country + " daily New Cases vs. Positive Rate per Test")
     plt.show()
 
+# %%
+### Gather features
+from sklearn.cluster import KMeans
+import numpy as np
+import pdb
+# Features: [max, min, median, mean] of [positive_rate, cardiovasc_death_rate, new_tests_smoothed_per_thousand, population_density, stringency index]
+all_countries = df["location"]
+all_countries.drop_duplicates(inplace=True)
+columns = ["positive_rate", "cardiovasc_death_rate", "new_tests_smoothed_per_thousand", "population_density", "stringency_index"]
+features = np.zeros([len(all_countries), 4 * len(columns)])
+for country_idx, country in enumerate(all_countries):
+    for idx, col in enumerate(columns):
+        country_data = df[df["location"] == country]
+        col_data = country_data[col]
+        stats = col_data.describe()
+        features[country_idx, idx] = stats["max"]
+        features[country_idx, idx + len(columns)] = stats["min"]
+        features[country_idx, idx + 2 * len(columns)] = stats["50%"]
+        features[country_idx, idx + 3 * len(columns)] = stats["mean"]
+print(features)
+
+#%%
+### Remove features with nan
+clean_features = np.zeros([1, 4 * len(columns)])
+clean_countries = []
+for count in range(len(all_countries)):
+    if np.sum(np.isnan(features[count])) == 0:
+        clean_features = np.append(clean_features, np.array([features[count]]), axis=0)
+        clean_countries.append(all_countries.iloc[count])
+clean_features = clean_features[1:, :]
+print(clean_features.shape)
+print(len(clean_countries))
+# %%
+### Normalize data
+means = np.mean(clean_features, axis=0)
+stds = np.std(clean_features, axis=0)
+normalized_features = (clean_features - means) / stds
+print(means.shape)
+
+### K-means
+num_clus = 10
+kmeans = KMeans(n_clusters=num_clus, random_state=0).fit(normalized_features)
+labels_per_country = kmeans.labels_
+print("Labels: ", labels_per_country)
+print("Cluster centers", kmeans.cluster_centers_)
+# %%
+### Stats
+groups = {}
+for idx in range(len(labels_per_country)):
+    if labels_per_country[idx] not in groups:
+        groups[labels_per_country[idx]] = [clean_countries[idx]]
+    else:
+        groups[labels_per_country[idx]].append(clean_countries[idx])
+print(groups)
 # %%
